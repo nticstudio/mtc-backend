f8235e03d684871089c84c7d7fc99e120102bd09
Macbook <nticstudio@gmail.com>
2022-04-19 20:36:16 +0200

    Fix entity note

--- src/Entity/Note.php
+++ src/Entity/Note.php
@@ -5,13 +5,20 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\NoteRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass=NoteRepository::class)
- */
+ * @Gedmo\SoftDeleteable()
+*/
 class Note
 {
+
+    use SoftDeleteableEntity;
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -29,6 +36,21 @@ class Note
      * @ORM\JoinColumn(nullable=false)
      */
     private $consult;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="notes" )
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $createdBy;
+
+
+      /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
 
     public function getId(): ?int
     {
@@ -55,6 +77,40 @@ class Note
     public function setConsult(?consult $consult): self
     {
         $this->consult = $consult;
+
+        return $this;
+    }
+
+     /**
+     * Sets createdAt.
+     *
+     * @return $this
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Returns createdAt.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function getCreatedBy(): ?User
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?User $createdBy): self
+    {
+        $this->createdBy = $createdBy;
 
         return $this;
     }
